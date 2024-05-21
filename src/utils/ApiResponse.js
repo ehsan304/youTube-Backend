b383b5790d2bdad8feee6b7986ba62256192316f
ehsan304 <muhammadehsan304@gmail.com>
2024-05-21 23:08:28 +0500

    update utils

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -9,25 +9,4 @@ class ApiResponse {
         this.message = message
         this.success = statusCode < 400
     }
-}class ApiError extends Error {
-    constructor(
-        statusCode,
-        message = "Something went wrong",
-        errors = [],
-        stack = ''
-
-    ){
-        super(message)
-        this.statusCode = statusCode
-        this.data = null
-        this.message = message
-        this.success = false
-        this.errors = this.errors
-
-        if (stack) {
-            this.stack = stack
-        } else {
-            Error.captureStackTrace(this, this.constructor)
-        }
-    }
 }
