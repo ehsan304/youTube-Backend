62408443da576b66b8903ade68837ba73ecb8207
ehsan304 <muhammadehsan304@gmail.com>
2024-05-22 15:47:47 +0500

    all setup configured

--- src/app.js
+++ src/app.js
@@ -25,7 +25,12 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// Routes
+import userRouter from './routes/user.routes.js'
 
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import express from 'express'
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv'
+
+const app = express()
+
+
 
 dotenv.config({
     path: './env'
