97e889b00f855de8aca5ed9cf1226fa26e96bb32
ehsan304 <muhammadehsan304@gmail.com>
2024-05-22 23:21:11 +0500

    update register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -21,7 +21,7 @@ const userSchema = new Schema({
         lowercase: true,
         trim: true
     },
-    username: {
+    fullname: {
         type: String,
         required: true,
         trim: true,
